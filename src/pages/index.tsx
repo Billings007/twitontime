d0a7306580486b8688cea705d8c6541a4993d04f
Parker Billings <parkerbillings@icloud.com>
2022-06-22 13:48:37 -0700

    edit tsconfig for paths

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen align-middle">
-      <h1 className="text-4xl font-extrabold">RAD Starter</h1>
+      <h1 className="text-4xl font-extrabold"></h1>
     </div>
   );
 };
