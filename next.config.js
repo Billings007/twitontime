a1fabddd46e71531bcaf4963fbc54f73f8e05ee4
Parker Billings <parkerbillings@icloud.com>
2022-06-22 13:37:09 -0700

    fixed tailwind

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    domains: [],
+  },
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+  experimental: { optimizeCss: true, legacyBrowsers: false, browsersListForSwc: true },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
