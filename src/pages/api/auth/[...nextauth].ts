1d7fc3bc4b274bd85742c52e92e4eaf56226f486
caffeinatedboi <89412826+caffeinatedboi@users.noreply.github.com>
2022-06-24 18:24:01 -0700

    Prisma updates

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,16 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
 import NextAuth from 'next-auth';
 import TwitterProvider from 'next-auth/providers/twitter';
 
+const prisma = new PrismaClient();
+
 const options = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     TwitterProvider({
-      clientId: process.env.CLIENT_ID as string,
-      clientSecret: process.env.CLIENT_SECRET as string,
+      clientId: process.env.TWITTER_CLIENT_ID as string,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
       version: '2.0',
     }),
   ],
