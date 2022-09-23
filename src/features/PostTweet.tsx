c3b83cbba4d12b8a95a7220931e71320b23191f2
caffeinatedboi <89412826+caffeinatedboi@users.noreply.github.com>
2022-09-23 14:59:30 -0400

    PostTweet + LogoutButton added

--- src/features/PostTweet.tsx
+++ src/features/PostTweet.tsx
@@ -1,10 +1,10 @@
-//import Input from '@components/input';
-//import { zodResolver } from '@hookform/resolvers/zod';
-//import { trpc } from '@utils/trpc';
-import { Control, SubmitHandler, useForm, useWatch } from 'react-hook-form';
-import { z } from 'zod';
+import TextAreaInput from '@components/TextArea';
+import { zodResolver } from '@hookform/resolvers/zod';
 import classNames from '@utils/classNames';
-
+import { trpc } from '@utils/trpc';
+import { useEffect } from 'react';
+import { Control, useForm, useWatch } from 'react-hook-form';
+import { z } from 'zod';
 
 const tweetSchema = z.object({
   tweetbody: z.string().min(1, { message: 'Tweet must be at least 1 characters' }),
@@ -12,8 +12,8 @@ const tweetSchema = z.object({
 
 type TweetSchema = z.output<typeof tweetSchema>;
 
-interface TweetProps{
-    userToken: string | undefined | unknown;
+interface TweetProps {
+  userToken: string | undefined | unknown;
 }
 
 function WatchTextArea({ control }: { control: Control<TweetSchema> }) {
@@ -38,4 +38,55 @@ function WatchTextArea({ control }: { control: Control<TweetSchema> }) {
   );
 }
 
-const 
+export default function PostTweet({ userToken }: TweetProps) {
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState,
+    formState: { errors },
+  } = useForm<TweetSchema>({
+    resolver: zodResolver(tweetSchema),
+    reValidateMode: 'onChange',
+    defaultValues: {
+      tweetbody: '',
+    },
+  });
+
+  const { isSuccess, isLoading, ...postRouter } = trpc.useMutation(['post']);
+
+  const onSubmit = async (data: TweetSchema) => {
+    if (data.tweetbody.length <= 280) {
+      await postRouter.mutateAsync({
+        tweetbody: data.tweetbody,
+        token: userToken as string,
+      });
+      formState.isSubmitSuccessful;
+    } else {
+      return ("Your tweet is too long!")
+    }
+  };
+
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset();
+    }
+  });
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextAreaInput
+        name="tweetbody"
+        error={errors.tweetbody}
+        defaultValue=""
+        value={formState.isSubmitSuccessful ? '' : undefined}
+        control={control}
+        label="Write tweet here"
+      />
+      <WatchTextArea control={control} />
+      <button type="submit" disabled={isLoading} className="px-2 py-3 mt-10 bg-white rounded-md">
+        {isSuccess ? 'Tweet Sent!' : 'Send Tweet'}
+      </button>
+    </form>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,25 @@
-import TextAreaInput from '@components/TextArea';
-import { zodResolver } from '@hookform/resolvers/zod';
-import classNames from '@utils/classNames';
-import { trpc } from '@utils/trpc';
+import LogoutButton from '@components/LogoutButton';
 import type { NextPage } from 'next';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 import Image from 'next/image';
-import { useEffect } from 'react';
-import { Control, SubmitHandler, useForm, useWatch } from 'react-hook-form';
 import DeleteTweet from 'src/features/DeleteTweet';
-import { z } from 'zod';
-
-const tweetSchema = z.object({
-  tweetbody: z.string().min(1, { message: 'Tweet must be at least 1 characters' }),
-});
-
-type TweetSchema = z.output<typeof tweetSchema>;
-
-function WatchTextArea({ control }: { control: Control<TweetSchema> }) {
-  const textArea = useWatch({
-    control,
-    defaultValue: '',
-    name: 'tweetbody',
-  });
-  const limit = textArea.length > 280;
-  const limitCaution = textArea.length > 220 && textArea.length < 280;
-
-  return (
-    <p
-      className={classNames(
-        limit ? 'text-red-400' : '',
-        limitCaution ? 'text-red-500' : 'text-white',
-        'text-right text-sm'
-      )}
-    >
-      {textArea.length}
-    </p>
-  );
-}
+import PostTweet from 'src/features/PostTweet';
+import SearchForm from 'src/features/SearchUser';
 
 const Home: NextPage = (props) => {
   const { data } = useSession();
-
-  //* New Router. isSuccess is a general boolean that we can use for conditional functions. For example, maybe we want to change the button text based on if the tweet was sent successfully. isSuccess ? 'Tweet Sent!' : 'Send Tweet'.//
-
-  const { isSuccess, isLoading, ...postRouter } = trpc.useMutation(['post']);
-
-  //* Optional schema validation. Technically we're doing this in server/tweet/index. You can even just create a schema folder with schemas like this, then import them into server/tweet/index */
-
-  //* Adding React Hook Forms, the general go-to form library right now. As simple as forms can seem, focus control, screen-reader support, and proper error handling can be an absolute nightmare to do without a library.*/
-
-  const {
-    control,
-    handleSubmit,
-    reset,
-    formState,
-    formState: { errors },
-  } = useForm<TweetSchema>({
-    resolver: zodResolver(tweetSchema),
-    reValidateMode: 'onChange',
-    defaultValues: {
-      tweetbody: '',
-    },
-  });
-
-  const handleTweetPost: SubmitHandler<TweetSchema> = async (values) => {
-    const validation = await tweetSchema.safeParseAsync(values);
-    if (!validation.success) {
-      return;
-    }
-    if (validation.success) {
-      if (data && data.accessToken) {
-        await postRouter.mutateAsync({
-          tweetbody: values.tweetbody,
-          token: data.accessToken as string,
-        });
-        formState.isSubmitSuccessful;
-      }
-    }
-  };
-
-  useEffect(() => {
-    if (formState.isSubmitSuccessful) {
-      reset();
-    }
-  });
 
   if (data?.user) {
     console.log(data.accessToken);
     const img = data.user.image as string;
     return (
       <div className="flex flex-col items-center justify-center h-screen align-middle">
-        <button
-          onClick={() => signOut()}
-          className="px-2 py-3 text-white border-2 border-white rounded-md"
-        >
-          Logout
-        </button>
         <p className="text-white">Hello {data.user.name}</p>
-
         <Image src={img} width="40" height="40" alt="" />
         <DeleteTweet userToken={data.accessToken} />
-
-        <form onSubmit={handleSubmit(handleTweetPost)} className="flex flex-col mt-10">
-          <TextAreaInput
-            name="tweetbody"
-            error={errors.tweetbody}
-            defaultValue=""
-            value={formState.isSubmitSuccessful ? '' : undefined}
-            control={control}
-            label="Write tweet here"
-          />
-          <WatchTextArea control={control} />
-          <button
-            type="submit"
-            disabled={isLoading}
-            className="px-2 py-3 mt-10 bg-white rounded-md"
-          >
-            {isSuccess ? 'Tweet Sent!' : 'Send Tweet'}
-          </button>
-        </form>
+        <SearchForm userToken={data.accessToken} />
+        <PostTweet userToken={data.accessToken} />
+        <LogoutButton userToken={data.accessToken} />
       </div>
     );
   }
