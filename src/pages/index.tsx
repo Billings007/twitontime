1cd09e938d1d2ad60f7c3b2fdeb38234615c7097
caffeinatedboi <89412826+caffeinatedboi@users.noreply.github.com>
2022-10-14 17:05:00 -0400

    update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,25 @@
 import type { NextPage } from 'next';
-import { signIn, useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 
-
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
   const { data } = useSession();
 
   if (data?.user) {
     console.log(data.accessToken);
     return (
       <div className="flex flex-col items-center justify-center h-screen align-middle">
-        <a className='justify-center flex-auto text-center text-large text-darkMode-4 '>Hi There! Use this page as a filler for the future!</a>
+        <a className="text-4xl font-extrabold text-darkMode-4">
+          Hi There! Use this page as a filler for the future!
+        </a>
       </div>
     );
   }
 
   return (
     <div className="flex flex-col items-center justify-center h-screen align-middle">
-      <h1 className="text-4xl font-extrabold text-darkMode-4">HI TWITTER</h1>
-      <button
-        onClick={() => signIn()}
-        className="px-2 py-3 text-white border-2 border-white rounded-md"
-      >
-        Login
-      </button>
+      <h1 className="text-4xl font-extrabold text-darkMode-4">
+        HI! Sign in using the button up top!
+      </h1>
     </div>
   );
 };
